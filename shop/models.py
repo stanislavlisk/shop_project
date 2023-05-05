11feba1dda7fa1b5c4d5b8a599d4f5ae37dfeae8
stasys <stazziss@gmail.com>
2023-05-05 15:11:08 +0300

    models, Item, migrated

--- shop/models.py
+++ shop/models.py
@@ -6,6 +6,7 @@ from datetime import date
 from PIL import Image
 from .my_utils import generate_thumbnail
 
+# Item = Product, can't change now! naming mistake
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -31,6 +32,30 @@ class ItemCategory(models.Model):
 class ItemModel(models.Model):
     item_model_name = models.CharField("Item model name", max_length=30, help_text="shop item category name")
     category_id = models.ManyToManyField("ItemCategory", help_text="Item category")
+    price = models.FloatField("Item price", default="0.00")
+
+class Item(models.Model):
+    item_model_id = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    ITEM_STATUS = (
+        ('u', 'Unavailable'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+        ('s', 'Sold'),
+    )
+
+    status = models.CharField(
+        "Status",
+        max_length=1,
+        choices=ITEM_STATUS,
+        blank=True,
+        default='u',
+        help_text="Leidinio kopijos statusas"
+    )
+
+
+
+
+
 
 
 
