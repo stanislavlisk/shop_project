99f4223b27f79fabdb21fff6fde78e42c15a9528
stasys <stazziss@gmail.com>
2023-05-06 16:29:20 +0300

    ItemModelDetailView

--- shop/urls.py
+++ shop/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path("administrator/<int:pk>/update_item_model", views.ItemModelUpdateView.as_view(), name='update_item_model_n'),
     path("administrator/<int:pk>/delete_item_model", views.ItemModelDeleteView.as_view(), name='delete_item_model_n'),
     path("view_items_models_list/", views.ItemModelListView.as_view(), name='view_items_models_list_n'),
-    path("view_item_model_detail/", views.ItemModelDetailView.as_view(), name='view_item_model_detail_n'),
+    path("view_item_model_detail/<int:pk>", views.ItemModelDetailView.as_view(), name='view_item_model_detail_n'),
     ##
     path("administrator/add_item", views.ItemCreateView.as_view(), name='add_item_n'),
     path("view_items/", views.ItemView.as_view(), name='view_items_n'),
