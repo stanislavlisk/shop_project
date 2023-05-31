0fb954bace8b1b2c99211d5b939bd0a7e81dc41d
stasys <stazziss@gmail.com>
2023-05-31 14:50:59 +0300

    >>>

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import secret as sec
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c=sjn26z4^l1==9&3efm0&q+#!46mrlqm&2j4apk8gfk23-2gh'
+SECRET_KEY = sec.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
