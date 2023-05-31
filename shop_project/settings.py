03b6fbe0c9344ff90c936ecf0464182691fcf55a
stasys <stazziss@gmail.com>
2023-05-31 17:52:00 +0300

    >>>

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = False
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
