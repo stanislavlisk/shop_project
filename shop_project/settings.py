4b26a3ee2e1e98e21f9146b7e6191f48873cd60a
stasys <stazziss@gmail.com>
2023-06-01 11:57:24 +0300

    >>>

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -12,12 +12,24 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
 from django.core.management.utils import get_random_secret_key
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+
+
 
 
 # Quick-start development settings - unsuitable for production
